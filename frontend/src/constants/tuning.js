e8a17eb3dfa9ac5292eb05d625064a52e84283ae
Rodelamo <sicochoco@gmail.com>
2025-07-01 16:33:08 -0600

    Add files via upload

--- frontend/src/constants/tuning.js
+++ frontend/src/constants/tuning.js
@@ -1,6 +1,4 @@
-// src/constants/tuning.js
-
-export const TUNING = {
+export const TUNING = Object.entries({
   1: 'F#4',
   2: 'D#4',
   3: 'G#4',
@@ -13,4 +11,11 @@ export const TUNING = {
   10: 'G#2',
   11: 'E2',
   12: 'B1'
-};
+}).map(([stringNum, noteWithOctave]) => {
+  const [note, octave] = noteWithOctave.match(/([A-G#]+)(\d)/).slice(1, 3);
+  return {
+    string: parseInt(stringNum),
+    note,
+    octave: parseInt(octave)
+  };
+});
